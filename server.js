7dec96aa22542ad41c49f72c77372de9568d041b
Mammy Sanyang <msanyang134@student.sfbu.edu>
2026-05-06 10:13:01 -0700

    Fix Render root route

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const fetch = (...args) =>
   import("node-fetch").then(({ default: fetch }) => fetch(...args));
@@ -10,6 +11,11 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 // Load environment variables
 const API_KEY = process.env.OPENAI_API_KEY;
